ca1252d2dcaf89e85c2ada8d44581214e87ccbe2
Hannah Galbraith <hannahrosegalbraith@gmail.com>
2018-11-24 15:49:08 -0800

    Moved function to better place in program

--- nlp.py
+++ nlp.py
@@ -68,6 +68,20 @@ def generate_padded_sequences(input_sequences):
 
 predictors, label, max_sequence_len = generate_padded_sequences(inp_sequences)
 
+def get_pretrained_embedding():
+    embeddings_index = dict()
+    f = open("glove_dir/glove.6B.100d.txt")
+    for line in f:
+        values = line.split()
+        word = values[0]
+        coefs = np.asarray(values[1:], dtype='float32')
+        embeddings_index[word] = coefs
+    f.close()
+    print('Loaded %s word vectors.' % len(embeddings_index))
+    return embeddings_index
+
+embeddings_index = get_pretrained_embedding()
+
 def create_model(max_sequence_len, total_words):
     input_len = max_sequence_len - 1
     model = Sequential()
@@ -90,20 +104,6 @@ model.summary()
 
 # train model
 model.fit(predictors, label, epochs=100, verbose=2)
-
-def get_pretrained_embedding():
-    embeddings_index = dict()
-    f = open("glove_dir/glove.6B.100d.txt")
-    for line in f:
-        values = line.split()
-        word = values[0]
-        coefs = np.asarray(values[1:], dtype='float32')
-        embeddings_index[word] = coefs
-    f.close()
-    print('Loaded %s word vectors.' % len(embeddings_index))
-    return embeddings_index
-
-embeddings_index = get_pretrained_embedding()
 
 def generate_text(seed_text, next_words, model, max_sequence_len):
     for _ in range(next_words):
